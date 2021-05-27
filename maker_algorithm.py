fd3a44bc62b7675d93c1c9bbcb2b488d18ef02bc
namkolgnottw <namkolgnottw@gmail.com>
2021-05-27 18:13:11 +0800

    traning set complete, test module complete

--- maker_algorithm.py
+++ maker_algorithm.py
@@ -2,9 +2,10 @@ import random
 from ml_oracle import train_model, file_to_dataframe, is_heavy
 from probables import CountMinSketch
 
+
 class RequestFile:
-    def __init__(self, file_size, file_type, is_in_homepage):
-        self.file_size = file_size
+    def __init__(self, update_year, file_type, is_in_homepage):
+        self.update_year = update_year
         self.file_type = file_type
         self.is_in_homepage = is_in_homepage
 
@@ -33,6 +34,7 @@ class makerCache:
         self.model = train_model()
         self.cms = CountMinSketch(width=1000, depth=5)
         self.hashtable = {} # single bucket for heavy items
+        self.miss_count = 0
 
     def print(self):
         for i in range(0, self.size) :
@@ -75,14 +77,14 @@ class makerCache:
 
     def evict(self, slot_pos):
        self.cache[slot_pos].page_name = '-'
-       self.cache[slot_pos].request_file.file_size = 0
+       self.cache[slot_pos].request_file.update_year = 0
        self.cache[slot_pos].request_file.file_type = '-'
        self.cache[slot_pos].request_file.is_in_hompage = False
        self.cache[slot_pos].marked = False
 
     def fill_in(self, slot_pos, new_page_name, request_file):
        self.cache[slot_pos].page_name = new_page_name
-       self.cache[slot_pos].request_file.file_size = request_file.file_size
+       self.cache[slot_pos].request_file.update_year = request_file.update_year
        self.cache[slot_pos].request_file.file_type = request_file.file_type
        self.cache[slot_pos].request_file.is_in_hompage = request_file.is_in_homepage
        self.cache[slot_pos].marked = True
@@ -101,43 +103,62 @@ class makerCache:
         self.evict(slot_pos)
         self.fill_in(slot_pos, new_page_name, request_file)
 
-    def select_unmarked(self):
+    def select_unmarked(self, mode):
         pos = 0
-        unmarked = {}
-        # rand method
+        unmarked = []
+        unmarked_freq = []
+
         for i in range(0, self.size):
             if self.cache[i].marked == False:
-              unmarked[i] = 0
-        #rand_pos = random.randint(0,len(unmarked)-1)
-        #print('rand_pos=', unmarked[rand_pos])
+              print(self.cache[i])
+              unmarked.append(i)
+              unmarked_freq.append(0)
 
-        print(unmarked)
-        # ml method
-        # predict all unmarked element (predict time = frequency from count-min sketch)
-        # select the lowest frequency unmarked element
-        for key, value in unmarked.items():
-            print('key:', key)
-            print('value:', value)
-            key0 = int(key)
-            freq = 0
-            # data preprocess
-            file_df = file_to_dataframe(self.cache[key0].request_file)
-            is_heavy0 = is_heavy(self.model, file_df)
+        print('unmarked:', unmarked)
+        # rand method
+        if mode=='random':
+          # print('using random method to select unmarked')
+          rand_pos = random.randint(0,len(unmarked)-1)
+          print('unmarked len =', len(unmarked))
+          print('rand_pos=', rand_pos)
+          print('real pos =', unmarked[rand_pos])
+          return unmarked[rand_pos]
 
-            if is_heavy0:
-                freq = self.hashtable[cache[key0].page_name]
-                print('freq:', freq)
-                unmarked[key0] = freq
+        elif mode=='ml_oracle':
+          # ml method
+          # predict all unmarked element (predict time = frequency from count-min sketch)
+          # select the lowest frequency unmarked element
+          # print('using ml oracle method to select unmarked')
+          for i in range(0, len(unmarked)):
 
-                # get from hash table
-            else:
-                # not heavy : get from count min sketch
-                freq = self.cms.check(self.cache[key0].page_name)
-                print('freq:', freq)
-                unmarked[key0] = freq
-        return pos
+              freq = 0
+              # data preprocess
+              file_df = file_to_dataframe(self.cache[unmarked[i]].request_file)
+              is_heavy0 = is_heavy(self.model, file_df)
 
-    def request_page(self, page_name, request_file):
+              if is_heavy0:
+                  freq = self.hashtable[self.cache[unmarked[i]].page_name]
+                  # print('freq:', freq)
+                  unmarked_freq[i] = freq
+
+                  # get from hash table
+              else:
+                  # not heavy : get from count min sketch
+                  freq = self.cms.check(self.cache[unmarked[i]].page_name)
+                  # print('freq:', freq)
+                  unmarked_freq[i] = freq
+          print(unmarked)
+          print(unmarked_freq)
+          max = 0
+          max_pos = 0
+          for j in range(0, len(unmarked)):
+            if unmarked[j] > max:
+                max = unmarked[j]
+                max_pos = j
+          return max_pos
+
+    def request_page(self, page_name, request_file, mode='random'):
+        # print('mode = ', mode)
         # requested page in cache
         if self.lookup(page_name) == True:
             self. set_marked(page_name)# set its marked = true
@@ -160,6 +181,9 @@ class makerCache:
             return True
 
         # page not in cache, idle slot available
+        # a miss occur
+        self.miss_count += 1
+
         if self.isAllSlotTaken() == False:
             for i in range(0, self.size):
                 if self.cache[i].page_name == '-':
@@ -172,7 +196,7 @@ class makerCache:
             return False
             
         # page not in cache and no idle slot
-        replace_pos = self.select_unmarked()
+        replace_pos = self.select_unmarked(mode)
         self.replace(replace_pos, page_name, request_file)
         if self.is_all_marked() == True:
             print('cache is all marked, ready to reset')
--- ml_oracle.py
+++ ml_oracle.py
@@ -21,7 +21,7 @@ def train_model():
   # Create target object and call it y
   y = data_set.whether_heavy
   # Create X
-  features = ['file_size', 'file_type', 'in_home_page']
+  features = ['update_year', 'file_type', 'in_home_page']
   X = data_set[features]
   #print(X)
   train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
@@ -32,15 +32,15 @@ def train_model():
   # Fit Model
   model.fit(train_X, train_y)
   # Make validation predictions and calculate mean absolute error
-  #val_predictions = model.predict(val_X)
+  val_predictions = model.predict(val_X)
   #print(val_predictions)
   #print(val_y)
-  #accuracy = metrics.accuracy_score(val_y, val_predictions)
-  #print(accuracy)
+  accuracy = metrics.accuracy_score(val_y, val_predictions)
+  print('accuracy : ', accuracy, end='\n')
   return model
 
 def file_to_dataframe(request_file):
-    file_dict = {'file_size':request_file.file_size, 
+    file_dict = {'update_year':request_file.update_year, 
                  'file_type':request_file.file_type,
                  'in_home_page':request_file.is_in_homepage
     }
@@ -52,8 +52,17 @@ def is_heavy(model, file_dataframe):
   labelencoder = LabelEncoder()
   file_dataframe['file_type'] = labelencoder.fit_transform(file_dataframe['file_type'])
   ret_df = model.predict(file_dataframe)
-  print("predict is_heavy =",ret_df)
+  # print("predict is_heavy =",ret_df)
   ret = ret_df[0]
-  print("predict is_heavy =",ret)
+  # print("predict is_heavy =",ret)
   return ret
 
+
+# for test model
+if __name__=='__main__':
+  while 1:
+     i = int(input('Run model test enter 1 , exit for 2\n\n'))
+     if i==1:
+       train_model()
+     elif i==2:
+       break
